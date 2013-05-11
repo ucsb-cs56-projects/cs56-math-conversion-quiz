23e41481565e7c1166769705fbb03e04c95e572b
Administrator <wenjie@ubuntu.(none)>
2013-05-11 00:11:07 -0700

    modify build.xml

---
+++ src/edu/ucsb/cs56/projects/math/cs56_math_conversion_quiz/QuestionTest.java
@@ -0,0 +1,183 @@
+package edu.ucsb.cs56.projects.math.cs56_math_conversion_quiz;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * 
+ * @author Andrew Berls
+ * @version CS56, Spring 2012, Mantis 0000611
+ * @see Question
+ *
+ */
+
+public class QuestionTest {
+	
+	/**
+	 * test constructor with num/radix arguments
+	 * @see Question#Question(int, int)
+	 */
+	@Test
+	public void testBinaryConstructor() {
+		Question q = new Question(50, 2);
+		assertEquals(50, q.getNum());
+		assertEquals("Binary", q.getStrRadix());
+	}
+	
+	/**
+	 * test setRadixes
+	 * @see Question#setRadixes()
+	 */
+	@Test
+	public void testSetRadixes() {
+		Question q = new Question(50, 2);
+		q.setRadixes(8);
+		assertEquals(8, q.getIntRadix());
+		assertEquals("Octal", q.getStrRadix());
+	}
+	
+	/**
+	 * test convertTo() -> binary
+	 * @see Question#convertTo(int)
+	 */
+	@Test
+	public void testConvertToBinary1() {
+		Question q = new Question(50, 2);
+		assertEquals("110010", q.convertTo(2));
+	}
+	
+	/**
+	 * test convertTo() -> octal
+	 * @see Question#convertTo(int)
+	 */
+	@Test
+	public void testConvertToOctal1() {
+		Question q = new Question(50, 8);
+		assertEquals("62", q.convertTo(8));
+	}
+	
+	/**
+	 * test convertTo() -> hex
+	 * @see Question#convertTo(int)
+	 */
+	@Test
+	public void testConvertToHex1() {
+		Question q = new Question(14, 16);
+		assertEquals("e", q.convertTo(16));
+	}
+	
+	/**
+	 * test convertTo() -> decimal
+	 * @see Question#convertTo(int)
+	 */
+	@Test
+	public void testConvertToDecimal1() {
+		Question q = new Question(15, 10);
+		assertEquals("1111", q.convertTo(2));
+	}
+	
+	/**
+	 * test convertTo() -> decimal
+	 * @see Question#convertTo(int)
+	 */
+	@Test
+	public void testConvertToDecimal2() {
+		Question q = new Question(15, 10);
+		assertEquals("f", q.convertTo(16));
+	}
+	
+	/**
+	 * test checkAnswer
+	 * @see Question#checkAnswer(String)
+	 */
+	@Test
+	public void testBinaryAnswer1() {
+		Question q = new Question(50, 2);
+		assertTrue(q.checkAnswer("110010"));
+	}
+	
+	/**
+	 * test checkAnswer
+	 * @see Question#checkAnswer(String)
+	 */
+	@Test
+	public void testBinaryAnswer2() {
+		Question q = new Question(78, 2);
+		assertTrue(q.checkAnswer("1001110"));
+	}
+	
+	/**
+	 * test checkAnswer
+	 * @see Question#checkAnswer(String)
+	 */
+	@Test
+	public void testOctalAnswer1() {
+		Question q = new Question(50, 8);
+		assertTrue(q.checkAnswer("62"));
+	}
+	
+	/**
+	 * test checkAnswer
+	 * @see Question#checkAnswer(String)
+	 */
+	@Test
+	public void testHexAnswer1() {
+		Question q = new Question(50, 16);
+		assertTrue(q.checkAnswer("32"));
+	}
+	
+	/**
+	 * test checkAnswer
+	 * @see Question#checkAnswer(String)
+	 */
+	@Test
+	public void testHexAnswer2() {
+		Question q = new Question(14, 16);		
+		assertTrue(q.checkAnswer("e"));
+	}
+	
+	/**
+	 * test sanitize - strip leading zeroes (1)
+	 * @see Question#sanitize(String s)
+	 */
+	 @Test
+	 public void testSanitize1() {
+	 Question q = new Question(46, 2);
+	  assertEquals("101110", q.sanitize("00101110"));
+	  assertTrue(q.checkAnswer("00101110"));
+	 }
+	 
+	 /**
+	 * test sanitize  -strip leading zeroes (2)
+	 * @see Question#sanitize(String s)
+	 */
+	 @Test
+	 public void testSanitize2() {
+	 Question q = new Question(2, 2);
+	  assertEquals("10", q.sanitize("000000000000010"));
+	  assertTrue(q.checkAnswer("000000000000010"));
+	 }
+	 
+	 /**
+	 * test sanitize - remove embedded spaces
+	 * @see Question#sanitize(String s)
+	 */
+	 @Test
+	 public void testSanitize3() {
+	 Question q = new Question(51, 2);
+	  assertEquals("110011", q.sanitize("110 011"));
+	  assertTrue(q.checkAnswer("110 011"));
+	 }
+	 
+	 /**
+	 * test sanitize - convert to lowercase
+	 * @see Question#sanitize(String s)
+	 */
+	 @Test
+	 public void testSanitize4() {
+	 Question q = new Question(42, 16);
+	  assertEquals("2a", q.sanitize("02A"));
+	  assertTrue(q.checkAnswer("02A"));
+	 }
+	
+}
---
+++ src/edu/ucsb/cs56/projects/math/cs56_math_conversion_quiz/QuizTest.java
@@ -0,0 +1,85 @@
+package edu.ucsb.cs56.projects.math.cs56_math_conversion_quiz;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 
+ * @author Andrew Berls
+ * @version CS56, Spring 2012, Mantis 0000611
+ * @see Quiz
+ *
+ */
+
+public class QuizTest {
+
+	/**
+	 * test numCorrect()
+	 * @see Quiz#numCorrect()
+	 * Seed a quiz with values and check numCorrect()
+	 */
+	@Test
+	public void testNumCorrect1() {
+		Quiz q = new Quiz(5);
+		q.insertScore(true);
+		q.insertScore(true);
+		q.insertScore(true);
+		q.insertScore(false);
+		q.insertScore(false);
+		assertEquals(3, q.numCorrect());
+	}
+	
+	/**
+	 * test numCorrect()
+	 * @see Quiz#numCorrect()
+	 * A quiz has no correct answers by default
+	 */
+	@Test
+	public void testNumCorrect2() {
+		Quiz q = new Quiz(5);		
+		assertEquals(0, q.numCorrect());
+	}
+	
+	/**
+	 * test getPercentage()
+	 * @see Quiz#getPercentage()
+	 * Test an entirely correct quiz
+	 */
+	@Test
+	public void testGetPercentage1() {
+		Quiz q = new Quiz(3);
+		q.insertScore(true);
+		q.insertScore(true);
+		q.insertScore(true);
+		assertEquals(100, q.getPercentage());
+	}
+	
+	/**
+	 * test getPercentage()
+	 * @see Quiz#getPercentage()
+	 * Test fractional rounding correctness
+	 */
+	@Test
+	public void testGetPercentage2() {
+		Quiz q = new Quiz(3);
+		q.insertScore(true);
+		q.insertScore(false);
+		q.insertScore(true);
+		assertEquals(67, q.getPercentage());
+	}
+	
+	/**
+	 * test getPercentage()
+	 * @see Quiz#getPercentage()
+	 */
+	@Test
+	public void testGetPercentage3() {
+		Quiz q = new Quiz(3);
+		q.insertScore(true);
+		q.insertScore(true);
+		q.insertScore(false);
+		q.insertScore(false);
+		assertEquals(50, q.getPercentage());
+	}
+	
+}
