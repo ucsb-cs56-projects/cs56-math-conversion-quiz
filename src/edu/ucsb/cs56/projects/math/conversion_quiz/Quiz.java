5ae8aca5b1bc7e376d9c20bc9f75a99663e2f640
Bohan Lin <bohan_lin@yahoo.com>
2013-05-18 23:34:33 -0700

    Fixed code, changed directories

---
+++ src/edu/ucsb/cs56/projects/math/conversion_quiz/Quiz.java
@@ -0,0 +1,139 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * A command line interface for a binary/octal/hex conversion Quiz.
+ * Original program by Erick Valle & George Barrios for Mantis 0000391
+ * @author Andrew Berls
+ * @version CS56, Spring 2012, Mantis 0000611
+ */
+
+public class Quiz {
+	
+	private ArrayList<Question> questions = new ArrayList<Question> ();
+    
+    private ArrayList<Boolean> scores = new ArrayList<Boolean>();
+    
+    /*
+     * mode is a magic number to represent the current quiz radix.
+     * For example, you can practice conversions to binary by setting it to 2
+     * The same is true of radixes 2, 8, 10, and 16
+     * -1 (default) is random mode: converts from any radix to any radix
+     */
+    private int mode;
+    
+    /**
+     * Constructor initializes a fixed number of random questions
+     * @param numQuestions The number of questions to initialize
+     */
+    public Quiz(int numQuestions) {
+    	this.mode = -1; // Random mode by default
+    	
+		for (int i=0; i<numQuestions; i++) {
+			// Generate a random Question and push it to the beginning of the list
+			Question q = new Question();
+			this.questions.add(0, q);
+		}
+	}
+    
+    public int getMode() {
+    	return this.mode;
+    }
+    
+    /**
+     * Set the mode to a specific radix, e.g., 2, 8, 10, 16
+     * in order to practice conversions to that radix
+     */
+    public void setMode(int mode) {
+    	this.mode = mode;
+    }
+    
+    public int getNumQuestions() {    	
+    	return this.questions.size();
+    }
+    
+    public Question getQuestion(int idx) {
+    	return this.questions.get(idx);
+    }
+    
+    /**
+     * Insert a score into the list of user answers
+     * @param correct A boolean indicating if the answer is correct
+     */
+    public void insertScore(boolean correct) {
+    	this.scores.add(0, correct);
+    }
+    
+    /**
+     * Get the number of correct answers from the scores array
+     * True indicates correct, false for incorrect
+     * @return The number of correct answers
+     */
+    public int numCorrect() {
+    	int result = 0;
+    	for (boolean correct : this.scores) {
+    		if (correct) {
+    			result++;
+    		}
+    	}
+    	return result;
+    }
+    
+    /**
+     * Get the user's percentage of correct answers
+     * @return The percentage of correct answers as an integer
+     */
+    public int getPercentage() {
+    	// numCorrect returns an integer, but must be coerced to a double
+    	// so that (numCorrect/size) works correctly
+    	
+    	double numCorrect = (double) this.numCorrect();
+    	int size = this.scores.size();
+    	double result = Math.round((numCorrect / size)*100);
+    	return (int) result;
+    }
+    
+    /**
+     * A readout showing the score percentage, e.g., "Your score was 67%!"
+     * @return A string readout
+     */
+    public String getReadout() {
+    	return "Your score was " + this.getPercentage() + "%!";
+    }  
+    
+    /**
+     * Run the Quiz game on the command line
+     * For each question, display its prompt and track the user's answer
+     * At the end, display the percentage of correct answers.
+     */
+    public void run() {
+    	Scanner scanner = new Scanner(System.in);
+    	for (Question q : this.questions) {
+    		System.out.println(q.generatePrompt());
+    		String userAnswer = scanner.next();
+    		
+    		if (q.checkAnswer(userAnswer)) {
+    			System.out.println("Correct!");
+    			this.insertScore(true);
+    		} else {    			
+    			System.out.println("Incorrect! Actual answer was: " + q.convertTo(q.getIntRadix()));
+    			this.insertScore(false);
+    		}
+    	}
+    	
+    	System.out.println("Your score was " + this.getPercentage() + "%!");
+    }
+	
+    /**
+     * main prompts the user for a number of questions and runs a new Quiz
+     * @param args Unused
+     */
+    public static void main (String [] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number of questions:");
+	    String s = scanner.next();
+		int numQuestions = Integer.parseInt(s);
+		Quiz quiz = new Quiz(numQuestions);
+		quiz.run();		
+    }
+}
---
+++ src/edu/ucsb/cs56/projects/math/conversion_quiz/QuizGui.java
@@ -0,0 +1,360 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+import java.awt.event.*;
+import java.io.File;
+/**
+ * GUI for binary/octal/hex conversion Quiz
+ * Original program by Erick Valle & George Barrios for Mantis 0000391
+ * @author Andrew Berls
+ * @version CS56, Spring 2012, Mantis 0000611
+ */
+
+public class QuizGui {
+
+    // Window components
+    JFrame frame = new JFrame("Binary/Octal/Hex Conversion Quiz");
+    // Used for creating/inserting vertical structs to act as spacer margins
+    Box box = new Box(BoxLayout.X_AXIS);
+
+    // Quiz-related variables
+    static QuizGui quizGui = new QuizGui();
+    private int numQuestions = 10;
+    Quiz quiz = new Quiz(numQuestions);	
+
+
+    // Sidebar references
+    JPanel sidebar = new JPanel();
+    JLabel currentQuestionNumLabel = new JLabel("Current Question: ");
+    JLabel currentQuestionNum = new JLabel(String.format("%d/%d", current+1, quiz.getNumQuestions()));
+    JLabel numCorrectLabel = new JLabel("Number Correct: ");
+    JLabel numCorrect = new JLabel(String.format("0/%d", quiz.getNumQuestions()));
+
+    JPanel modePanel = new JPanel();
+    JLabel practiceLabel = new JLabel("I want to practice:");
+    JButton binaryMode = new JButton("Binary Mode");
+    JButton octalMode = new JButton("Octal Mode");
+    JButton decimalMode = new JButton("Decimal Mode");
+    JButton hexadecimalMode = new JButton("Hexadecimal Mode");
+    JButton randomMode = new JButton("Random Mode");
+
+    // Content references
+    JPanel content = new JPanel();
+    JPanel userInput = new JPanel();
+    JLabel questionLabel = new JLabel("");
+    JTextField userAnswer = new JTextField(25);
+
+    JButton submit = new JButton("Submit");
+
+    JPanel results = new JPanel();
+    JLabel feedback = new JLabel("");
+
+    JPanel scorePanel = new JPanel();
+    JLabel scoreReadout = new JLabel("");
+    JButton tryAgain = new JButton("Try Again!");
+
+    JPanel animationPanel = new JPanel();
+
+
+    // Specific question references
+    private static int current;
+    private Question currentQuestion = new Question();
+    
+    /**
+     * Build the Quiz GUI window
+     */
+    public QuizGui build() {
+	guiRemoveAll();	
+
+	questionLabel.setPreferredSize(new Dimension(200, 20));
+
+	int bottomMargin = 15;
+
+	//---------------------
+	//-- Sidebar
+	//---------------------
+	sidebar.setLayout(new BoxLayout(sidebar, BoxLayout.Y_AXIS));
+
+	sidebar.add(currentQuestionNumLabel);
+	sidebar.add(currentQuestionNum);
+
+	sidebar.add(box.createVerticalStrut(bottomMargin));
+
+	sidebar.add(numCorrectLabel);
+	sidebar.add(numCorrect);
+
+	modePanel.add(box.createVerticalStrut(bottomMargin));
+
+	modePanel.setLayout(new BoxLayout(modePanel, BoxLayout.Y_AXIS));
+
+	modePanel.add(practiceLabel);
+
+	modePanel.add(box.createVerticalStrut(bottomMargin-5));
+
+	binaryMode.addActionListener(new binaryModeListener());
+	modePanel.add(binaryMode);
+
+	modePanel.add(box.createVerticalStrut(bottomMargin-10));
+
+	octalMode.addActionListener(new octalModeListener());
+	modePanel.add(octalMode);
+
+	modePanel.add(box.createVerticalStrut(bottomMargin-10));
+
+	decimalMode.addActionListener(new decimalModeListener());
+	modePanel.add(decimalMode);
+
+	modePanel.add(box.createVerticalStrut(bottomMargin-10));
+
+	hexadecimalMode.addActionListener(new hexadecimalModeListener());
+	modePanel.add(hexadecimalMode);
+
+	modePanel.add(box.createVerticalStrut(bottomMargin-10));
+
+	randomMode.addActionListener(new randomModeListener());
+	modePanel.add(randomMode);
+
+	sidebar.add(modePanel);
+
+	frame.getContentPane().add(BorderLayout.EAST, sidebar);
+
+	//---------------------
+	//-- Main Content
+	//---------------------
+
+	// userInput sub-pane
+	userInput.setLayout(new BoxLayout(userInput, BoxLayout.Y_AXIS));
+	userInput.add(questionLabel);
+	userInput.add(box.createVerticalStrut(5));
+
+
+	userInput.add(userAnswer); // ----------------------------------------------
+
+	userInput.add(box.createVerticalStrut(5));
+
+	submit.addActionListener(new submitListener());
+	userInput.add(feedback);
+	userInput.add(box.createVerticalStrut(5));
+	userInput.add(submit);
+
+	// Results sub-pane
+	results.setLayout(new BoxLayout(results, BoxLayout.Y_AXIS));
+
+	// Score readout sub-pane (Visible at end)
+	scorePanel.setLayout(new BoxLayout(scorePanel, BoxLayout.Y_AXIS));
+	scorePanel.add(scoreReadout);
+	scorePanel.add(box.createVerticalStrut(10));
+	tryAgain.addActionListener(new tryAgainListener());
+	scorePanel.add(tryAgain);
+	scorePanel.setVisible(false); // Enabled at end of quiz
+	frame.getContentPane().add(BorderLayout.CENTER, scorePanel);
+
+	// Attach sub-panes to content pane
+	content.add(userInput);
+	content.add(results);
+	frame.getContentPane().add(BorderLayout.WEST, content);
+
+
+	//---------------------
+	//-- Window setup
+	//---------------------
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(640, 480);
+	frame.setVisible(true);
+
+	return this; // For chaining method calls
+    }
+
+    /**
+     * When the user clicks submit, send feedback on their answer
+     * and update appropriate counters
+     */
+    class submitListener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {	
+	    String answer = userAnswer.getText();
+
+	    if (currentQuestion.checkAnswer(answer)) {
+		feedback.setText("Correct!");
+		correctAnimation();
+		quiz.insertScore(true);
+	    } else {
+		feedback.setText("Incorrect! Answer was: " + currentQuestion.getAnswer());
+		quiz.insertScore(false);
+	    }
+
+	    String numCorrectStr = String.format("%d/%d", quiz.numCorrect(), quiz.getNumQuestions());
+	    numCorrect.setText(numCorrectStr);
+	    current++;
+	    currentQuestionNum.setText(String.format("%d/%d", current+1, quiz.getNumQuestions()));
+
+	    currentQuestion = new Question();
+	    quizGui.ask();
+	}
+    }
+
+    /**
+     * Try Again button - run a new Quiz on click
+     */
+    class tryAgainListener implements ActionListener {
+	public void actionPerformed (ActionEvent e) {
+	    quiz = new Quiz(numQuestions);
+
+	    // Re-enable quiz inputs
+	    numCorrect.setText((String.format("0/%d", quiz.getNumQuestions())));
+	    sidebar.setVisible(true);
+	    userInput.setVisible(true);
+	    scorePanel.setVisible(false);
+	    feedback.setText("");
+
+	    // Restart the quiz
+	    current = 0;
+	    currentQuestionNum.setText(String.format("%d/%d", current+1, quiz.getNumQuestions()));
+	    quizGui.ask();
+	}
+    }
+
+    /**
+     * Set the quiz to practice converting only to binary
+     */
+    class binaryModeListener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+	    feedback.setText("");
+	    currentQuestion = new Question(currentQuestion.getRandomNum(), 2);
+	    quiz.setMode(2);	
+	    quizGui.ask();
+	}
+    }
+
+    /**
+     * Set the quiz to practice converting only to octal
+     */
+    class octalModeListener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+	    quiz.setMode(8);	
+	    feedback.setText("");
+	    currentQuestion = new Question(currentQuestion.getRandomNum(), 8);
+	    quizGui.ask();
+	}
+    }
+
+    /**
+     * Set the quiz to practice converting only to decimal
+     */
+    class decimalModeListener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+	    quiz.setMode(10);
+	    feedback.setText("");
+	    currentQuestion = new Question(currentQuestion.getRandomNum(), 10);	
+	    quizGui.ask();
+	}
+    }
+
+    /**
+     * Set the quiz to practice converting only to hexadecimal
+     */
+    class hexadecimalModeListener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+	    quiz.setMode(16);
+	    feedback.setText("");
+	    currentQuestion = new Question(currentQuestion.getRandomNum(), 16);	
+	    quizGui.ask();	
+	}
+    }
+
+    /**
+     * Set the quiz back to random conversions
+     */
+    class randomModeListener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+	    quiz.setMode(-1);	
+	    currentQuestion = new Question();	
+	    quizGui.ask();
+	}
+    }
+
+    /**
+     * Update fields to ask the current question, and end the quiz if necessary
+     */
+    public void ask() {
+	userAnswer.setText(""); // Clear the text field
+
+	if (current >= quiz.getNumQuestions()) {
+	    // If we're through with the questions, hide the inputs and show the final readout
+	    sidebar.setVisible(false);
+	    userInput.setVisible(false);
+	    scoreReadout.setText(quiz.getReadout());
+	    scorePanel.setVisible(true);
+	} else {
+	    // Else ask the current question
+	    String prompt = currentQuestion.generatePrompt(quiz.getMode());
+	    questionLabel.setText(prompt);
+	}
+    }
+    public class correctImages extends JPanel implements ActionListener{
+        private javax.swing.Timer animator;
+        private ImageIcon imageArray[];
+        private int delay = 300;
+        private int total = 12;
+        private int currentImage = 0;
+        public correctImages(){
+            imageArray = new ImageIcon[total];
+            for(int i = 0; i < imageArray.length; i++){
+		// imageArray[i] = new ImageIcon("C:\\Users\\Bohan Lin\\Desktop\\images\\image"+ i + ".jpg");
+		imageArray[i] = new ImageIcon("~/cs/student/bohan_lin/cs56/proj01/cs56-math-conversion-quiz/images/image" + i + ".jpg");
+            }
+            animator = new javax.swing.Timer(delay,this);
+            animator.start();
+        }
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            if(currentImage >= imageArray.length){
+                currentImage = 0;
+            }
+            currentImage++;
+            imageArray[currentImage].paintIcon(this, g, 0, 0);
+        }
+        public void actionPerformed(ActionEvent e){
+            repaint();
+        }
+    }
+    public void correctAnimation(){
+        guiRemoveAll();
+        JPanel cancelPanel = new JPanel(new BorderLayout());
+        cancelPanel.setSize(100,50);
+        JButton cancel = new JButton("Go to next question!");
+	cancel.setPreferredSize(new Dimension(50,50));
+	cancel.addActionListener(new cancelListener());
+	cancelPanel.add(cancel);
+
+        correctImages cImages = new correctImages(); 
+        frame.getContentPane().add(BorderLayout.CENTER, cImages);
+        frame.getContentPane().add(BorderLayout.SOUTH, cancelPanel);
+	frame.setBackground(Color.WHITE);
+	frame.setVisible(true);
+        frame.setSize(714,400);
+    }
+    class cancelListener implements ActionListener{
+	public void actionPerformed(ActionEvent event){
+	    guiRemoveAll();
+	    build();
+	}
+    }
+    public void guiRemoveAll() {
+	sidebar.removeAll();
+	modePanel.removeAll();
+	content.removeAll();
+	userInput.removeAll();
+	results.removeAll();
+	scorePanel.removeAll();
+	frame.getContentPane().removeAll();
+	frame.validate();
+	frame.repaint();
+    }
+    /**
+     * Build and run the GUI
+     */
+    public static void main (String [] args) {	
+	quizGui.build().ask();
+    }
+
+}
